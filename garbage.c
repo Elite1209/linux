228b2ed8a2bc4ead88fef9410c28883fe4d43741
Elite1209 <koulrajshri@gmail.com>
2021-02-17 22:05:12 +0000

    10.5

--- garbage.c
+++ garbage.c
@@ -2503,11 +2503,10 @@
 
 // #include<stdio.h>
 // #include<math.h>
-// #define square(x) x*x
 // float Edistance(int x1,int x2,int y1,int y2)
 // {
 //     float distance;
-//     distance = square(x2-x1)+square(y2-y1);
+//     distance = ((x2-x1) * (x2-x1)) + ((y2 -y1) * (y2-y1));
 //     return sqrt(distance);
 // }
 // int main (){
@@ -2527,36 +2526,5 @@
 
 // return 0;
 // }
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-float Edistance(int x1, int y1, int x2, int y2)
-{
-    int a = (y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1);
-    return sqrt(a);
-}
-int main()
-{
-    int x1, y1, x2, y2;
-    float dst;
-    printf("Enter the value of x1\n");
 
-    scanf("%d", &x1);
-
-    printf("Enter the value of x2\n");
-
-    scanf("%d", &x2);
-
-    printf("Enter the value of y1\n");
-
-    scanf("%d", &y1);
-
-    printf("Enter the value of y2\n");
-
-    scanf("%d", &y2);
-
-    dst = Edistance(x1, y1, x2, y2);
-    printf("The distance between these points is %.2f\n", dst);
-    return 0;
-}
